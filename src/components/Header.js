6e0fb75bbb9094712779f573af6201c3db43e47c
annakulova <aakulovaa03@gmail.com>
2024-07-03 13:25:45 +0300

    first edit for header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div className='header__titel'>
+            <div>
+                <span className='logo'>OrgTex</span>
+            </div>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
