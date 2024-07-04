a1b39fe775bd07df5fdc87cd616f7f1f8b59f1a1
annakulova <aakulovaa03@gmail.com>
2024-07-04 14:55:13 +0300

    add 'summa' shop-cart'

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import Order from "./Order";
 
 const showOrders = (props) => {
+    let summa=0
+    props.orders.forEach(el => summa += Number.parseInt(el.price))
     return (
         <div>
             {props.orders.map(el => (
                             <Order onDelete={props.onDelete} key={el.id} item={el} />
-                        ))}
+            ))}
+            <p className='summa'>Сумма: {new Intl.NumberFormat().format(summa)}Р</p>
         </div>
     )
 }
