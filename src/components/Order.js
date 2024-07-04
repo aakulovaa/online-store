d8b43cd86f46013322943e1fb92d055624563612
annakulova <aakulovaa03@gmail.com>
2024-07-04 14:58:15 +0300

    edit 'shop-cart'

--- src/components/Order.js
+++ src/components/Order.js
@@ -4,15 +4,12 @@ import { FaTrash } from 'react-icons/fa'
 export default class Order extends Component {
   render() {
     return (
-        <main>
-        <div className='catalog__name'>Корзина</div>
         <div className='item'>
             <img src={"./img/" + this.props.item.img}/>
             <h2>{this.props.item.title}</h2>
             <b>{this.props.item.price}P</b>
             <FaTrash className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)}/>
         </div>
-      </main>
     )
   }
 }
