c0404c0dadb695b325e90eec7561d00205c2b14b
annakulova <aakulovaa03@gmail.com>
2024-08-01 18:05:51 +0300

    made supporting chat

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,13 +1,60 @@
-import React, { Component } from 'react'
+import React, {useEffect, useState} from 'react'
+import {addDoc, collection, serverTimestamp, onSnapshot, query, doc, orderBy} from 'firebase/firestore'
+import {db, auth} from "../firebase"
 
-// Класс, описывающий чат поддержки
+export default function Support() {
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([])
+  const messagesRef = collection(db,"messages")
 
-export default class Support extends Component {
-  render() {
-    return (
-      <div className='support'>
-        <h2 className='support__title'>Поддержка</h2>
-      </div>
-    )
-  }
+  useEffect(() => {
+    const queryMessages = query(messagesRef, orderBy("createdAt"))
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({...doc.data(), id: doc.id});
+      });
+
+      setMessages(messages)
+    });
+    return () => unsuscribe();
+  },[]);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if (newMessage === "") return;
+    
+    await addDoc(messagesRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      photo: auth.currentUser.photoURL,
+    });
+
+    setNewMessage("")
+  };
+
+  return (
+    <div className='support'>
+      <h2 className='support__title'>Поддержка</h2>
+      <div className='messages'>
+        {messages.map((message) => (
+            <div className={`message ${auth.currentUser.uid ? 'sent' : 'received'}`} key={message.id}>
+            <img src={message.photo} alt='user-photo' className='user-photo'/>
+            <span className='user'>{message.user}</span>
+            <p className='message-text'>{message.text}</p>
+          </div>
+        ))}
+        </div>
+      <form onSubmit={handleSubmit} className='new-message-form'>
+        <textarea className='new-message-input'
+        placeholder='Введите сообщение'
+        onChange={(e) => setNewMessage(e.target.value)}
+        value={newMessage}
+        />
+        <button tupe="submit" className='send-button'>Отправить</button>
+      </form>
+    </div>
+  )
 }
+
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
-
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,3 +24,4 @@ const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 export {provider};
+export const db = getFirestore(app)
